d34b5a2ba7bce0b84a9e8b5ab1d2b72d17e211e9
Ssnig <kyawsanh402@gmail.com>
2026-01-01 22:59:32 +0630

    HomePage Ui Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,11 @@ const Header = () => {
             <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden'
                 style={{ backgroundImage: "url(/header_img.png)" }}
                 id='Header'>
-               <div>
-                <h2>Explore Home That Fits Your Dream</h2>
-                <div>
-                    <a href="">Projects</a>
-                    <a href="">contact Us</a>
+               <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white '>
+                <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore Home That Fits Your Dream</h2>
+                <div className='space-x-6 mt-16' >
+                    <a href="" className='border border-white px-6 py-3 rounded'>Projects</a>
+                    <a href="" className='bg-blue-500 px-6 py-3 rounded'>Contact Us</a>
                 </div>
                </div>
                 
